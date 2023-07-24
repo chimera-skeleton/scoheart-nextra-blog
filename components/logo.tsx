74b901db27354704391b03c6443eafce664f585a
Scoheart <Scoheart1487790559@gmail.com>
2023-07-24 19:57:01 +0800

    feat: initial commit

---
+++ components/logo.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import type { ReactElement } from "react";
+import style from "./logo.module.scss";
+
+export default function Logo(): ReactElement {
+    return (
+        <div className={style.wrapper}>
+            <Image
+                className={style.logo}
+                src="/scoheart.svg"
+                alt="deno"
+                width={40}
+                height={40}
+            ></Image>
+            <h1 className={style.title}>Heart ☘️ Sco</h1>
+        </div >
+    )
+}
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,60 @@
 import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import type { DocsThemeConfig } from 'nextra-theme-docs'
+import Logo from './components/logo';
+import style from "./theme.module.css";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: (<Logo></Logo>),
+  primaryHue: 275,
+  useNextSeoProps() {
+    return {
+      titleTemplate: 'Scoheart Blog'
+    }
+  },
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
+  banner: {
+    dismissible: false,
+    key: '2.0-release',
+    text: (
+      <a href="https://github.com/scoheart" target="_blank">
+        🎉 Scoheart Github. Read more →
+      </a>
+    )
   },
+  sidebar: {
+    defaultMenuCollapseLevel: 1,
+    autoCollapse: false,
+    toggleButton: true,
+    titleComponent({ title, type }) {
+      if (type === 'separator') {
+        return (
+          <div className={style.title}>{title}</div>
+        )
+      }
+      return <>{title}</>
+    }
+  },
+  i18n: [
+    { locale: 'en', text: 'English' },
+    { locale: 'zh', text: '中文' },
+    { locale: 'de', text: 'Deutsch' },
+    { locale: 'ar', text: 'العربية', direction: 'rtl' }
+  ],
+  gitTimestamp: (
+    <>
+      <h1>demo</h1>
+    </>
+  ),
+  docsRepositoryBase: 'https://github.com/scoheart',
+  footer: {
+    text: "Scoheart's Blog",
+  },
+  faviconGlyph: "👻"
 }
 
 export default config
